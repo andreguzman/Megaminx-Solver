0c74b7c8d5c4f8788e0391feb813f65bb2abf23d
Brian Liu <b_liu72@yahoo.com>
2014-09-27 19:07:50 -0400

    Face Edge piece overhaul

--- Megaminx Solver/src/Megaminx.java
+++ Megaminx Solver/src/Megaminx.java
@@ -1,21 +1,9 @@
 import java.util.ArrayList;
 
 public class Megaminx {
-	ArrayList<Face> minx = new ArrayList<Face>();
+	E
 
-	public Megaminx(Face a, Face b, Face c, Face d, Face e, Face f, Face g,
-			Face h, Face i, Face j, Face k, Face l) {
-		minx.add(a);
-		minx.add(b);
-		minx.add(c);
-		minx.add(d);
-		minx.add(e);
-		minx.add(f);
-		minx.add(g);
-		minx.add(h);
-		minx.add(i);
-		minx.add(j);
-		minx.add(k);
-		minx.add(l);
+	public Megaminx(){
+		
 	}
 }
