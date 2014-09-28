a8b0fa81ccdcf02fa8e9d773bef8131cac749306
Brian Liu <b_liu72@yahoo.com>
2014-09-27 20:28:32 -0400

    Getting Errors to work

--- Megaminx Solver/src/Megaminx.java
+++ Megaminx Solver/src/Megaminx.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 
 public class Megaminx {
 	EdgePiece[] e = new EdgePiece[30];
@@ -19,34 +19,34 @@ public class Megaminx {
 	public int getCornerNumber(int f1, int f2, int f3){
 		
 		switch(f1+f2+f3){
-			case:3 return 0;
-			case:6 return 1;
-			case:5 return 2;
-			case:7 return 3;
-			case:9 
+			case 3: return 0;
+			case 6: return 1;
+			case 5: return 2;
+			case 7: return 3;
+			case 9: 
 				if (f1 == 0 || f2 == 0 || f3 == 0)
 					return 4;
 				return 5;
-			case:16 return 6;
-			case:17 
+			case 16: return 6;
+			case 17: 
 				if (f1 == 1 || f2 == 1 || f3 == 1)
 					return 7;
 				return 8;
-			case:12 return 9;
-			case:15
+			case 12: return 9;
+			case 15:
 				if (f1 == 2 || f2 == 2 || f3 == 2)
 					return 10;
 				return 11;
-			case:18 return 12;
-			case:21 return 13;
-			case:24
+			case 18: return 12;
+			case 21: return 13;
+			case 24:
 				if (f1 == 5 || f2 == 5 || f3 == 5)
 					return 14;
 				return 15;
-			case:27 return 16;
-			case:26 return 17;
-			case:28 return 18;
-			case:30 return 19;
+			case 27: return 16;
+			case 26: return 17;
+			case 28: return 18;
+			case 30: return 19;
 		
 		
 		
